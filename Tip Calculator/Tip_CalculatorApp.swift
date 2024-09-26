ac182f12f13c93685bafbbcbc3cff682b9536994
plxl <210124508+plxl@users.noreply.github.com>
2024-09-26 16:39:28 +0800

    Initial Commit

---
+++ Tip Calculator/Tip_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tip_CalculatorApp.swift
+//  Tip Calculator
+//
+//  Created by Jamie on 26/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Tip_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Tip CalculatorTests/Tip_CalculatorTests.swift
@@ -0,0 +1,16 @@
+//
+//  Tip_CalculatorTests.swift
+//  Tip CalculatorTests
+//
+//  Created by Jamie on 26/9/24.
+//
+
+import Testing
+
+struct Tip_CalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
